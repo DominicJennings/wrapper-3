02eda6da03dcc30e42a4b0fa9097c5bf66dcfdeb
Dominic Jennings <149413413+DominicJennings@users.noreply.github.com>
2024-04-13 18:55:35 -0600

    Update page.js

--- static/page.js
+++ static/page.js
@@ -143,7 +143,7 @@ module.exports = function (req, res, url) {
 					themeId: "custom",
 					tlang: "en_US",
 					presaveId: presave,
-					goteam_draft_only: 1,
+					goteam_draft_only: 0,
 					isWide: 1,
 					collab: 0,
 					nextUrl: "/html/list/movies.html",
